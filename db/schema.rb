138bad86f1ba00fad77c1463e559e2b33793e8ee
dz3jkob <jtrzebinski@gmail.com>
2015-11-03 11:18:05 +0100

    tweaks to cards - tested

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151029203643) do
+ActiveRecord::Schema.define(version: 20151103080857) do
+
+  create_table "cards", force: :cascade do |t|
+    t.string   "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "user_id"
+  end
+
+  add_index "cards", ["user_id"], name: "index_cards_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
